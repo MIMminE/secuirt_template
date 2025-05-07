7edbb49d31bf4cad603470e09496caca6bf4fc1a
MIMminE <weluv.ww@gmail.com>
2025-05-07 09:38:21 +0900

    project init

---
+++ src/test/java/nuts/spring_template/security_template/SecurityTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package nuts.spring_template.security_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
